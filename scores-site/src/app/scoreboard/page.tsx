54a296c016c0907c14bacd33ee70e21050092a22
adharsh242001 <163978878+adharsh242001@users.noreply.github.com>
2025-10-17 11:56:33 +0530

    FInal changes

--- scores-site/src/app/scoreboard/page.tsx
+++ scores-site/src/app/scoreboard/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <Scoreboard
       title="Live Scores"
       logoSrc="/Logo.png"                     // put your logo in /public
-      brandColor="#732dff85"                    // any color (e.g., emerald)
+      brandColor="#00000085"                    // any color (e.g., emerald)
       csvUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vTotvCpeAiaoYBIhx4WoO86wUQJ_ITeeTDuUPBEVs2V8PNlCLP7C1qvloOY3v15owLXFdznb1AioC95/pub?gid=680379986&single=true&output=csv"
       // Or, if private:
       // apiKey={process.env.NEXT_PUBLIC_GSHEETS_API_KEY!}
